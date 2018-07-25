382413bd0cb71acdb77327824ead1dd50b318ef5
Karolik <36989581+Karolik@users.noreply.github.com>
2018-07-25 23:07:47 +0200

    Update Map.js

--- src/Map.js
+++ src/Map.js
@@ -86,7 +86,18 @@ export default class Map extends Component {
         });
       }
     }
-  
+    
+    makeMarkerIcon = (markerColor) => {
+      const {google} = this.props
+      let markerImage = new google.maps.MarkerImage(
+        'http://chart.googleapis.com/chart?chst=d_map_spin&chld=1.15|0|'+ markerColor +
+        '|40|_|%E2%80%A2',
+        new google.maps.Size(21, 34),
+        new google.maps.Point(0, 0),
+        new google.maps.Point(10, 34),
+        new google.maps.Size(21,34));
+      return markerImage;
+    }
 
   render() {
     return(
