a8429c0fd5bd774671139a557435a5c64e0edf70
Karolik <36989581+Karolik@users.noreply.github.com>
2018-08-06 17:20:39 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,26 @@
 import React from 'react';
 //import { GoogleApiWrapper } from 'google-maps-react'
 import './App.css'
-import Map from './Map'
+import MapContainer from './MapContainer'
 
 function MapsApp(props) {
-    return (
-      <div className="app">
+  return (
+    <div className="app">
+      <header className="App-header">
+        <a id="menu" className="header_menu">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+            <path d="M2 6h20v3H2zm0 5h20v3H2zm0 5h20v3H2z"/>
+          </svg>
+        </a>
         <h1 className="title"> Notre Dame, Versailles </h1>
-        <Map google={props.google} />
-      </div>
-    )
-  }
-
-/*export default GoogleApiWrapper({
-  apiKey: 'AIzaSyDs0VIWSmdG3BZnOiBxOWz4SVqQF0t7QmQ'
-})(MapsApp)*/
+      </header>
+      <main>
+        <MapContainer google={props.google} 
+        //onToggle={ this.toggleAside }
+        />
+      </main>
+    </div>
+  )
+}
 
 export default MapsApp;
