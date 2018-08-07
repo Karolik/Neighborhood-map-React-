4fc3d2bf990b04f2d038de3828308a544172300d
Karolik <36989581+Karolik@users.noreply.github.com>
2018-08-07 15:38:47 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,17 +6,16 @@ import MapContainer from './MapContainer'
 function MapsApp(props) {
   return (
     <div className="app">
-      <header className="App-header">
-        <a id="menu" className="header_menu">
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+      <header className="app-header" >
+        <a id="menu" className="header-menu" tabIndex="0">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" tabIndex="0">
             <path d="M2 6h20v3H2zm0 5h20v3H2zm0 5h20v3H2z"/>
           </svg>
         </a>
         <h1 className="title"> Notre Dame, Versailles </h1>
       </header>
       <main>
-        <MapContainer google={props.google} 
-        //onToggle={ this.toggleAside }
+        <MapContainer google={props.google}
         />
       </main>
     </div>
