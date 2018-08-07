7f8fda09da7766b8f1be83b863811c4028391958
Karolik <36989581+Karolik@users.noreply.github.com>
2018-08-07 21:13:53 +0200

    Update Search.js

--- src/Search.js
+++ src/Search.js
@@ -3,7 +3,7 @@ import 'whatwg-fetch';
 
 const Search = (props) => {
   return (
-    <input className="search" role="search" type='text'
+    <input className="search" role="search" type="text"
       aria-label="Search and filter venues by name"
       value={props.query}
       onChange={(event) => props.searchVenues(event.target.value)}
