2d69d5fe03cfd23725c717658c9b040751ac0ec7
Karolik <36989581+Karolik@users.noreply.github.com>
2018-08-07 21:08:09 +0200

    Update ListView.js

--- src/ListView.js
+++ src/ListView.js
@@ -1,11 +1,12 @@
-import React from 'react';
+import React from 'react'
 import 'whatwg-fetch';
 
 const ListView = (props) => {
+
     return (
         <ul className="listView">
-        {props.markers.filter(m => m.getVisible()).map((m, i) =>
-            (<li key={i} tabIndex="0">{m.title}</li>))
+        {props.markers.filter(marker => marker.getVisible()).map((marker, i) =>
+            (<li key={i}><button>{marker.title}</button></li>))
         }
         </ul>
     )
