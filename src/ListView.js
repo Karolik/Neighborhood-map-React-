f7ad2bb2b28d6343bd3aeb87d11431a22089025f
Karolik <36989581+Karolik@users.noreply.github.com>
2018-08-07 21:12:40 +0200

    Update ListView.js

--- src/ListView.js
+++ src/ListView.js
@@ -6,7 +6,7 @@ const ListView = (props) => {
     return (
         <ul className="listView">
         {props.markers.filter(marker => marker.getVisible()).map((marker, i) =>
-            (<li key={i}><button>{marker.title}</button></li>))
+            (<li key={i} aria-label="Top pick on the map"><button aria-label="Click to open more info">{marker.title}</button></li>))
         }
         </ul>
     )
